956cc24bd132f5bf3900431a6d086b8290a0cec6
Venkat Yadlapati <venkat.yadlapati3620@gmail.com>
2019-07-03 02:57:37 +0000

    jasmine_sprout: Intial bringup

--- dot_jasmine_sprout.mk
+++ dot_jasmine_sprout.mk
@@ -21,21 +21,19 @@
 # definition file).
 #
 #TWRP
-BUILD_TWRP := true
+#BUILD_TWRP := true
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/jasmine_sprout/device.mk)
 
-# Inherit from Havoc custom product configuration
-$(call inherit-product, vendor/havoc/config/common.mk)
-
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.havoc.maintainer=rcstar6696
-
+# Inherit from  custom product configuration
+$(call inherit-product, vendor/dot/config/common.mk)
+export DOT_OFFICIAL := true
+TARGET_BOOT_ANIMATION_RES := 1080
 # Device identifier
 PRODUCT_BRAND := xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_PLATFORM := SDM660
-PRODUCT_NAME := havoc_jasmine_sprout
+PRODUCT_NAME := dot_jasmine_sprout
 PRODUCT_DEVICE := jasmine_sprout
 PRODUCT_MODEL := Mi A2
 
